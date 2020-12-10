51f94066da14c6f317d5cf2f5de7a24cd0ed96ad
xzhan50 <joan.zhang@emory.edu>
2020-12-10 12:33:20 -0500

    save dev work; add discussion items

--- R/allele-haplotype.R
+++ R/allele-haplotype.R
@@ -11,6 +11,22 @@
 #' }
 #' @export
 ht <- function(dat_in){
+  # discussion
+  # 1. which frequency table we are going to use
+  # 2. algorithm of filter
+  # 3. output? format and topN?
+  #   4. genotype typing resolution score?
+  #
+  #   my steps:
+  #   1. data clean(seperate typing by before/after colon for comparison)
+  # 2. for each allele,
+  # - if it's 4 digits, then pull out row index of rows have the 4 digits code
+  #  - if it's 2 digits, then pull out rows index of rows start with the first 2 digits
+  # 3. union the indices, subset the raw frequency table
+  # 4. keep
+  # - all allele combination if non-missing input alleles are subset of the records
+  # - allele combination from step3 but filtered by rank of each population
+  #
   # ori <- read.csv("~/projects/DEV/haplostats_dev/data/csv/A~C~B~DRB1~DQB1.csv") %>%
   #   select(A, C, B, DRB1, DQB1,
   #          AFA_freq, AFA_rank, API_freq, API_rank,CAU_freq, CAU_rank, HIS_freq, HIS_rank, NAM_freq, NAM_rank ) %>%
@@ -139,30 +155,61 @@ ht <- function(dat_in){
   #   l[[i]] <- tmpFunc(dat[i,])
   # }
   #
-  # ck1 <- l[[1]]
-  # ck2 <- l[[2]]
+  # re1 <- l[[1]]
+  # re2 <- l[[2]]
   #
-  # # check ck1
+  # final1 <- tempfunc(datin1 = dat[1,], datin2 = re1)
+  # final2 <- tempfunc(datin1 = dat[2,], datin2 = re2)
   #
-  # tmp <- dat[1,]
-  # colnms <- tmp %>%
-  #   select(id, fstA, lstA, fstB, lstB, fstC, lstC, fstDRB1, lstDRB1, fstDQB1, lstDQB1) %>%
-  #   mutate(across(c(fstA, lstA, fstB, lstB, fstC, lstC, fstDRB1, lstDRB1, fstDQB1, lstDQB1), is.na)) %>%  # replace all NA with TRUE and else FALSE
-  #   pivot_longer(-id, names_to = "var") %>%  # pivot longer
-  #   filter(value != 1) %>%
-  #   pull(var)
+  # final <- rbind(final1, final2)
   #
-  # ck_ck1 <- ck1 %>% left_join(., tmp, by = colnms) %>%
-  #   filter(!is.na(id)) %>%
-  #   # filter_at(vars(AFA_rank, API_rank, CAU_rank, HIS_rank, NAM_rank), all_vars(!is.na(.))) %>%
-  #   filter_at(vars(AFA_rank, API_rank, CAU_rank, HIS_rank, NAM_rank), any_vars(!is.na(.))) %>%
-  #   select(c(A, C, B, DRB1, DQB1, AFA_freq, AFA_rank, API_freq, API_rank, CAU_freq, CAU_rank, HIS_freq, HIS_rank, NAM_freq, NAM_rank))
+  # tempfunc <- function(datin1, datin2){
   #
-  # ck1 <- ck1 %>%
-  #   mutate(flag = ifelse(AFA_rank %in% ck_ck1$AFA_rank[!is.na(ck_ck1$AFA_rank)] |
-  #                          API_rank %in% ck_ck1$API_rank[!is.na(ck_ck1$API_rank)] |
-  #                          CAU_rank %in% ck_ck1$CAU_rank[!is.na(ck_ck1$CAU_rank)] |
-  #                          HIS_rank %in% ck_ck1$HIS_rank[!is.na(ck_ck1$HIS_rank)] |
-  #                          NAM_rank %in% ck_ck1$NAM_rank[!is.na(ck_ck1$NAM_rank)], "yes", "no"))
+  #   # pull out alleles info if column is not empty
+  #   colnms <- datin1 %>%
+  #     select(id, fstA, lstA, fstB, lstB, fstC, lstC, fstDRB1, lstDRB1, fstDQB1, lstDQB1) %>%
+  #     mutate(across(c(fstA, lstA, fstB, lstB, fstC, lstC, fstDRB1, lstDRB1, fstDQB1, lstDQB1), is.na)) %>%  # replace all NA with TRUE and else FALSE
+  #     pivot_longer(-id, names_to = "var") %>%  # pivot longer
+  #     filter(value != 1) %>%
+  #     pull(var)
+  #
+  #   re_w_alleles <- datin2 %>% left_join(., datin1, by = colnms) %>%
+  #     filter(!is.na(id)) %>%
+  #     # filter_at(vars(AFA_rank, API_rank, CAU_rank, HIS_rank, NAM_rank), all_vars(!is.na(.))) %>%
+  #     filter_at(vars(AFA_rank, API_rank, CAU_rank, HIS_rank, NAM_rank), any_vars(!is.na(.))) %>%
+  #     select(c(A, C, B, DRB1, DQB1, AFA_freq, AFA_rank, API_freq, API_rank, CAU_freq, CAU_rank, HIS_freq, HIS_rank, NAM_freq, NAM_rank))
+  #
+  #   datin2 <- datin2 %>%
+  #     mutate(flag = ifelse(AFA_rank %in% re_w_alleles$AFA_rank[!is.na(re_w_alleles$AFA_rank)] |
+  #                            API_rank %in% re_w_alleles$API_rank[!is.na(re_w_alleles$API_rank)] |
+  #                            CAU_rank %in% re_w_alleles$CAU_rank[!is.na(re_w_alleles$CAU_rank)] |
+  #                            HIS_rank %in% re_w_alleles$HIS_rank[!is.na(re_w_alleles$HIS_rank)] |
+  #                            NAM_rank %in% re_w_alleles$NAM_rank[!is.na(re_w_alleles$NAM_rank)], "yes", "no"))
+  #
+  #   re_full <- datin2 %>% filter(flag == "yes") %>%
+  #     select(c(A, C, B, DRB1, DQB1, AFA_freq, AFA_rank, API_freq, API_rank, CAU_freq, CAU_rank, HIS_freq, HIS_rank, NAM_freq, NAM_rank)) %>%
+  #     filter_at(vars(AFA_rank, API_rank, CAU_rank, HIS_rank, NAM_rank), any_vars(!is.na(.))) %>%
+  #     gather(., pop, poprank, c(AFA_rank, API_rank, CAU_rank, HIS_rank, NAM_rank)) %>%
+  #     group_by(pop) %>%
+  #     mutate(rank = row_number()) %>%
+  #     ungroup() %>%
+  #     filter(!is.na(poprank)) %>%
+  #     mutate(note = "full")
+  #
+  #   re_comb <- datin2 %>% filter(flag == "no") %>%
+  #     select(c(A, C, B, DRB1, DQB1, AFA_freq, AFA_rank, API_freq, API_rank, CAU_freq, CAU_rank, HIS_freq, HIS_rank, NAM_freq, NAM_rank)) %>%
+  #     filter_at(vars(AFA_rank, API_rank, CAU_rank, HIS_rank, NAM_rank), any_vars(!is.na(.))) %>%
+  #     gather(., pop, poprank, c(AFA_rank, API_rank, CAU_rank, HIS_rank, NAM_rank)) %>%
+  #     filter(!is.na(poprank)) %>%
+  #     group_by(pop) %>%
+  #     mutate(rank = row_number()) %>%
+  #     ungroup() %>%
+  #     filter(rank <= 100) %>%
+  #     mutate(note = "comb")
+  #
+  #   re <- rbind(re_full, re_comb)
+  #   return(re)
+  # }
+  #
 }
 
