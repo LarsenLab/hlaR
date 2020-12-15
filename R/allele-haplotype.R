7fa414bf97eb313235eace2f125e5057785891d0
xzhan50 <joan.zhang@emory.edu>
2020-12-15 16:40:36 -0500

    main function of haplotype with drb345 added

--- R/allele-haplotype.R
+++ R/allele-haplotype.R
@@ -20,10 +20,10 @@
 
 CompHaploTbl <- function(dat_in, cut_p, cut_r){
   #* step 1: import raw haplotype frequenc table and do a brief cleaning *#
-    # raw_hap_tbl <- read.csv("~/projects/DEV/haplostats_dev/data/csv/A_C_B_DRB1_DQB1.csv") %>%
-    raw_hap_tbl <- read.csv(system.file("extdata", "A_C_B_DRB1_DQB1.csv", package = "hlaR"), check.names = FALSE) %>%
+  # raw_hap_tbl <- read.csv("~/projects/hlaR/inst/extdata/A_C_B_DRB345_DRB1_DQB1.csv") %>%
+  raw_hap_tbl <- read.csv(system.file("extdata", "A_C_B_DRB345_DRB1_DQB1.csv", package = "hlaR"), check.names = FALSE) %>%
     rename_all(. %>% tolower) %>%
-    select(a, c, b, drb1, dqb1,
+    select(a, c, b, drb1, dqb1, drb345,
            afa_freq, afa_rank, api_freq, api_rank, cau_freq, cau_rank, his_freq, his_rank, nam_freq, nam_rank) %>%
     mutate(idx = as.numeric(rownames(.))) %>%
     # remove trailing g
@@ -31,14 +31,16 @@ CompHaploTbl <- function(dat_in, cut_p, cut_r){
            b = ifelse(str_detect(b, "g"), str_replace(b, "g", ""), b),
            c = ifelse(str_detect(c, "g"), str_replace(c, "g", ""), c),
            drb1 = ifelse(str_detect(drb1, "g"), str_replace(drb1, "g", ""), drb1),
-           dqb1 = ifelse(str_detect(dqb1, "g"), str_replace(dqb1, "g", ""), dqb1)) %>%
+           dqb1 = ifelse(str_detect(dqb1, "g"), str_replace(dqb1, "g", ""), dqb1),
+           drb345 = ifelse(str_detect(drb345, "g"), str_replace(drb345, "g", ""), drb345)) %>%
     # remove leading loci letter
     mutate(a = str_sub(a, start = 3),
            b = str_sub(b, start = 3),
            c = str_sub(c, start = 3),
            drb1 = str_sub(drb1, start = 6),
-           dqb1 = str_sub(dqb1, start = 6)) %>%
-    # split allele by : for comparison with input data
+           dqb1 = str_sub(dqb1, start = 6),
+           drb345 = str_sub(drb345, start = 6)) %>%
+    # split allele by ":" for comparison with input data
     mutate(fst_a = sub("\\:.*", "", a),
            lst_a = sub(".*\\:", "", a),
            fst_b = sub("\\:.*", "", b),
@@ -48,24 +50,26 @@ CompHaploTbl <- function(dat_in, cut_p, cut_r){
            fst_drb1 = sub("\\:.*", "", drb1),
            lst_drb1 = sub(".*\\:", "", drb1),
            fst_dqb1 = sub("\\:.*", "", dqb1),
-           lst_dqb1 = sub(".*\\:", "", dqb1))
+           lst_dqb1 = sub(".*\\:", "", dqb1),
+           fst_drb345 = sub("\\:.*", "", drb345),
+           lst_drb345 = sub(".*\\:", "", drb345))
   #* end of step 1 *#
 
   #* step 2: rechape input data table by recipient and donor *#
   rcpt <- dat_in %>%
-           rename_all(. %>% tolower) %>%
-           select(contains(c("id", "ethnicity","race","rcpt"))) %>%
-           rename_at(vars(contains('rcpt_')), list(~sub('rcpt_', '', .))) %>%
-           mutate(id = paste("rcpt", rowid, sep = "_"),
-                  a1 = ifelse(nchar(a1) == 1, paste0("0", a1), a1)) %>%
-           select(id, everything())
+    rename_all(. %>% tolower) %>%
+    select(contains(c("id", "ethnicity","race","rcpt"))) %>%
+    rename_at(vars(contains('rcpt_')), list(~sub('rcpt_', '', .))) %>%
+    mutate(id = paste("rcpt", rowid, sep = "_"),
+           a1 = ifelse(nchar(a1) == 1, paste0("0", a1), a1)) %>%
+    select(id, everything())
 
   don <- dat_in %>%
-          rename_all(. %>% tolower) %>%
-          select(contains(c("id", "ethnicity","race","don"))) %>%
-          rename_at(vars(contains('don_')), list(~sub('don_', '', .))) %>%
-          mutate(id = paste("don", rowid, sep = "_")) %>%
-          select(id, everything())
+    rename_all(. %>% tolower) %>%
+    select(contains(c("id", "ethnicity","race","don"))) %>%
+    rename_at(vars(contains('don_')), list(~sub('don_', '', .))) %>%
+    mutate(id = paste("don", rowid, sep = "_")) %>%
+    select(id, everything())
 
   dat2 <- rbind(rcpt, don) %>% arrange(rowid) %>%
     mutate_all(as.character) %>%
@@ -78,7 +82,9 @@ CompHaploTbl <- function(dat_in, cut_p, cut_r){
            drb1 = ifelse(nchar(drb1) == 1, paste0("0", drb1), drb1),
            drb2 = ifelse(nchar(drb2) == 1, paste0("0", drb2), drb2),
            dqb1 = ifelse(nchar(dqb1) == 1, paste0("0", dqb1), dqb1),
-           dqb2 = ifelse(nchar(dqb2) == 1, paste0("0", dqb2), dqb2))
+           dqb2 = ifelse(nchar(dqb2) == 1, paste0("0", dqb2), dqb2),
+           drb345_1 = ifelse(nchar(drb345_1) == 1, paste0("0", drb345_1), drb345_1),
+           drb345_2 = ifelse(nchar(drb345_2) == 1, paste0("0", drb345_2), drb345_2))
 
   rm(rcpt, don)
   #* end of step 2 *#
@@ -90,6 +96,7 @@ CompHaploTbl <- function(dat_in, cut_p, cut_r){
   for (i in 1:num_subj){
     result[[i]] <- FuncForCompHaplo(tbl_in1 = raw_hap_tbl, tbl_in2 = dat2[i, ], cut_freq = cut_p, cut_rank = cut_r)
   }
+
   #* end of step 3 *#
   names(result) <- dat2$id
   return(result)
