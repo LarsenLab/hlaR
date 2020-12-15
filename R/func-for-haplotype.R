7a884ed11fab4f9cbed6fdd2a743e287595e4bec
xzhan50 <joan.zhang@emory.edu>
2020-12-15 13:06:42 -0500

    add drb345

--- R/func-for-haplotype.R
+++ R/func-for-haplotype.R
@@ -40,6 +40,11 @@ FuncForCompHaplo <- function(tbl_in1, tbl_in2, cut_freq = 0.0001, cut_rank = 10)
     tmp_indx <- c(tmp_indx, tmp)
   }
 
+  tmp <- tbl_in1 %>% filter(fst_drb345 %in% c(tbl_in2$drb345_1, tbl_in2$drb345_2)) %>% pull(idx)
+  if (length(tmp > 0)){
+    tmp_indx <- c(tmp_indx, tmp)
+  }
+
   tmp_indx <- unique(tmp_indx)
 
   if(length(tmp_indx) > 0) {
@@ -50,10 +55,11 @@ FuncForCompHaplo <- function(tbl_in1, tbl_in2, cut_freq = 0.0001, cut_rank = 10)
                cnt_b = ifelse(fst_b %in% c(tbl_in2$b1, tbl_in2$b2), 1, 0),
                cnt_c = ifelse(fst_c %in% c(tbl_in2$c1, tbl_in2$c2), 1, 0),
                cnt_drb = ifelse(fst_drb1 %in% c(tbl_in2$drb1, tbl_in2$drb2), 1, 0),
-               cnt_dqb = ifelse(fst_dqb1 %in% c(tbl_in2$dqb1, tbl_in2$dqb2), 1, 0)) %>%
-        mutate(cnt = cnt_a + cnt_b + cnt_c + cnt_drb + cnt_dqb) %>%
+               cnt_dqb = ifelse(fst_dqb1 %in% c(tbl_in2$dqb1, tbl_in2$dqb2), 1, 0),
+               cnt_drb345 = ifelse(fst_drb345 %in% c(tbl_in2$drb345_1, tbl_in2$drb345_2), 1, 0)) %>%
+        mutate(cnt = cnt_a + cnt_b + cnt_c + cnt_drb + cnt_dqb + cnt_drb345) %>%
         filter(cnt > 1 & !is.na(cau_rank)) %>%
-        select(id, idx, a, b, c, drb1, dqb1, cau_freq, cau_rank, cnt) %>%
+        select(id, idx, a, b, c, drb1, dqb1, drb345, cau_freq, cau_rank, cnt) %>%
         arrange(cnt, cau_rank) %>%
         group_by(cnt) %>%
         mutate(rank = 1:n()) %>% # rank of each count group
@@ -68,10 +74,11 @@ FuncForCompHaplo <- function(tbl_in1, tbl_in2, cut_freq = 0.0001, cut_rank = 10)
                cnt_b = ifelse(fst_b %in% c(tbl_in2$b1, tbl_in2$b2), 1, 0),
                cnt_c = ifelse(fst_c %in% c(tbl_in2$c1, tbl_in2$c2), 1, 0),
                cnt_drb = ifelse(fst_drb1 %in% c(tbl_in2$drb1, tbl_in2$drb2), 1, 0),
-               cnt_dqb = ifelse(fst_dqb1 %in% c(tbl_in2$dqb1, tbl_in2$dqb2), 1, 0)) %>%
-        mutate(cnt = cnt_a + cnt_b + cnt_c + cnt_drb + cnt_dqb) %>%
+               cnt_dqb = ifelse(fst_dqb1 %in% c(tbl_in2$dqb1, tbl_in2$dqb2), 1, 0),
+               cnt_drb345 = ifelse(fst_drb345 %in% c(tbl_in2$drb345_1, tbl_in2$drb345_2), 1, 0)) %>%
+        mutate(cnt = cnt_a + cnt_b + cnt_c + cnt_drb + cnt_dqb + cnt_drb345) %>%
         filter(cnt > 1 & !is.na(afa_rank)) %>%
-        select(id, idx, a, b, c, drb1, dqb1, afa_freq, afa_rank, cnt) %>%
+        select(id, idx, a, b, c, drb1, dqb1, drb345, afa_freq, afa_rank, cnt) %>%
         arrange(cnt, afa_rank) %>%
         group_by(cnt) %>%
         mutate(rank = 1:n()) %>%
