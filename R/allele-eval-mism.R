3027b8d342a9dd516cfd8de4fc18f7498abca17a
xzhan50 <joan.zhang@emory.edu>
2021-01-26 10:41:34 -0500

    add both of low-res and high-res flags

--- R/allele-eval-mism.R
+++ R/allele-eval-mism.R
@@ -9,7 +9,7 @@
 #' @param recip_2
 #' recipient's alpha2 or beta1 domain
 #' @return
-#' a data frame with cleaned donor and recipients, and donor to recipient mis-match flags
+#' a data frame with cleaned donor and recipients, and mis-match flags on low and high resolution alleles of donor to recipient
 #' @export
 #'
 #' @import
@@ -26,10 +26,10 @@
 EvalAlleleMism <- function(don_1, don_2, recip_1, recip_2)
 {
   # start of calling CleanAllele() to clean hla value #
-  don_1_clean <- unlist(CleanAllele(don_1, don_2)[1])
-  don_2_clean <- unlist(CleanAllele(don_1, don_2)[2])
-  recip_1_clean <- unlist(CleanAllele(recip_1, recip_2)[1])
-  recip_2_clean <- unlist(CleanAllele(recip_1, recip_2)[2])
+  don_1_clean <- CleanAllele(don_1, don_2)$locus1_clean
+  don_2_clean <- CleanAllele(don_1, don_2)$locus2_clean
+  recip_1_clean <- CleanAllele(recip_1, recip_2)$locus1_clean
+  recip_2_clean <- CleanAllele(recip_1, recip_2)$locus2_clean
 
   tmp <- as.data.frame(cbind(don_1_clean, don_2_clean, recip_1_clean, recip_2_clean)) %>%
     mutate_all(as.character)
@@ -37,27 +37,37 @@ EvalAlleleMism <- function(don_1, don_2, recip_1, recip_2)
 
   # start of calculating mis-match of donor's hla in recipient #
   len <- length(don_1_clean)
-  mis_1 <- numeric(length = len)
-  mis_2 <- numeric(length = len)
+  mis_1_lo <- numeric(length = len)
+  mis_2_lo <- numeric(length = len)
+
+  mis_1_hi <- numeric(length = len)
+  mis_2_hi <- numeric(length = len)
 
   for (i in 1:len)
   {
     # if both alleles are NA in donor or recipient, then set mis-match value to NA
-    if((is.na(tmp[i,1]) & is.na(tmp[i,2])) | (is.na(tmp[i,3]) & is.na(tmp[i,4]))){
-      mis_1[i] <- NA
-      mis_2[i] <- NA
+    # if((is.na(tmp[i,1]) & is.na(tmp[i,2])) | (is.na(tmp[i,3]) & is.na(tmp[i,4]))){
+    if((tmp[i,1] == "" & tmp[i,2] == "") | (tmp[i,3] == "" & tmp[i,4] == "")){
+      mis_1_lo[i] <- NA
+      mis_2_lo[i] <- NA
+      mis_1_hi[i] <- NA
+      mis_2_hi[i] <- NA
     }
     # if all of allele are high resolution
     else if(length(tmp[i,1]) == 5 & length(tmp[i,2]) == 5 & length(tmp[i,3]) == 5 & length(tmp[i,4]) == 5){
       if(tmp[i,1] %in% c(tmp[i,3],tmp[i,4])){
-        mis_1[i] <- 0}
+        mis_1_lo[i] <- NA
+        mis_1_hi[i] <- 0}
       else{
-        mis_1[i] <- 1}
+        mis_1_lo[i] <- NA
+        mis_1_hi[i] <- 1}
 
       if(tmp[i,2] %in% c(tmp[i,3],tmp[i,4])){
-        mis_2[i] <- 0}
+        mis_2_lo[i] <- NA
+        mis_2_hi[i] <- 0}
       else{
-        mis_2[i] <- 1}
+        mis_2_lo[i] <- NA
+        mis_2_hi[i] <- 1}
     }
     else{
       tmp[i,1] <- str_sub(tmp[i,1],1,2)
@@ -66,14 +76,18 @@ EvalAlleleMism <- function(don_1, don_2, recip_1, recip_2)
       tmp[i,4] <- str_sub(tmp[i,4],1,2)
 
       if(tmp[i,1] %in% c(tmp[i,3],tmp[i,4])){
-        mis_1[i] <- 0}
+        mis_1_lo[i] <- 0
+        mis_1_hi[i] <- NA}
       else{
-        mis_1[i] <- 1}
+        mis_1_lo[i] <- 1
+        mis_1_hi[i] <- NA}
 
       if(tmp[i,2] %in% c(tmp[i,3],tmp[i,4])){
-        mis_2[i] <- 0}
+        mis_2_lo[i] <- 0
+        mis_2_hi[i] <- NA}
       else{
-        mis_2[i] <- 1}
+        mis_2_lo[i] <- 1
+        mis_2_hi[i] <- NA}
     }
   }
   # end of calculating mis-match #
@@ -82,7 +96,8 @@ EvalAlleleMism <- function(don_1, don_2, recip_1, recip_2)
 
   result <- data.frame(don_1_clean = don_1_clean, don_2_clean = don_2_clean,
                        recip_1_clean = recip_1_clean, recip_2_clean = recip_2_clean,
-                       mism_1 = mis_1, mism_2 = mis_2)
+                       mism_1_lo = mis_1_lo, mism_2_lo = mis_2_lo,
+                       mism_1_hi = mis_1_hi, mism_2_hi = mis_2_hi)
   rownames(result) <- seq(1, dim(result)[1])
 
   return(result)
