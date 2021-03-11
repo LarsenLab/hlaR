9bdb4ee472b16ae6e84772d35d3b71a1b6d698a2
xzhan50 <joan.zhang@emory.edu>
2021-03-11 12:51:30 -0500

    prep-mmv2-mhcII-ref-table: add 78VS/33N/78Y/108P/85V to the mmv2 reference table

--- R/prep-mmv2-mhcII-ref-table.R
+++ R/prep-mmv2-mhcII-ref-table.R
@@ -13,7 +13,9 @@ library(janitor)
 # notes:
 # 1. eplet names:
 #    - all converted to upper cases (DQA1*04:02 and DQA1*04:04 were 187a, DQA1*05:01 and DQA1*01:02 were 187A. all of them are 187A in current version)
-# 2.
+# 2. add a list of updated eplets from epregistry.com
+#    - antibody reactive: 78VS(==98ES)
+#    - non-antibody reactive: 33N, 78Y, 108P, 85V
 
 #* function to pull out eplets *#
 PullEplet <- function(filename, sheetname){
@@ -146,10 +148,132 @@ r <- as_tibble(t(r), rownames = "type") %>%
       replace(., is.na(.), "") %>%
       arrange(type, index)
 
- # write.csv(b, "inst/extdata/matchmaker/check/mhcII_v2_B.csv", row.names = FALSE)
+# write.csv(mmv2_b, "inst/extdata/matchmaker/check/mhcII_v2_B.csv", row.names = FALSE)
 #* mmv2_a and mmv2_b have been validated, code above are stable *#
 
 #* add missing eplets to the table *#
+#* eplet lists
+`33N` <- c("DRB1*01:01", "DRB1*01:02", "DRB1*01:03", "DRB1*03:01", "DRB1*03:02", "DRB1*03:03", "DRB1*07:01", "DRB1*08:01",
+ "DRB1*08:02", "DRB1*09:01", "DRB1*09:02", "DRB1*10:01", "DRB1*11:01", "DRB1*11:03", "DRB1*11:04", "DRB1*12:01",
+ "DRB1*12:02", "DRB1*13:01", "DRB1*13:02", "DRB1*13:03", "DRB1*13:05", "DRB1*14:01", "DRB1*14:02", "DRB1*14:03",
+ "DRB1*14:04", "DRB1*14:54", "DRB1*15:01", "DRB1*15:02", "DRB1*15:03", "DRB1*16:01", "DRB1*16:02", "DRB3*01:01",
+ "DRB3*02:01", "DRB3*02:02", "DRB3*03:01", "DRB4*01:01", "DRB4*01:03", "DRB5*01:01", "DRB5*02:02")
+
+`78Y` <- c("DRB1*01:01", "DRB1*01:02", "DRB1*01:03", "DRB1*03:01", "DRB1*03:02", "DRB1*03:03", "DRB1*04:01", "DRB1*04:02",
+           "DRB1*04:03", "DRB1*04:04", "DRB1*04:05", "DRB1*08:01", "DRB1*08:02", "DRB1*10:01", "DRB1*11:01", "DRB1*11:03",
+           "DRB1*11:04", "DRB1*12:01", "DRB1*12:02", "DRB1*13:01", "DRB1*13:02", "DRB1*13:03", "DRB1*13:05", "DRB1*14:01",
+           "DRB1*14:02", "DRB1*14:03", "DRB1*14:04", "DRB1*14:54", "DRB1*15:01", "DRB1*15:02", "DRB1*15:03", "DRB1*16:01",
+           "DRB1*16:02", "DRB3*01:01", "DRB3*02:01", "DRB3*02:02", "DRB3*03:01", "DRB4*01:01", "DRB4*01:03", "DRB5*01:01",
+           "DRB5*02:02")
+
+`108P` <- c("DRB1*01:01", "DRB1*01:02", "DRB1*01:03", "DRB1*03:01", "DRB1*03:02", "DRB1*03:03", "DRB1*04:01", "DRB1*04:02",
+            "DRB1*04:03", "DRB1*04:04", "DRB1*04:05", "DRB1*07:01", "DRB1*08:01", "DRB1*08:02", "DRB1*09:01", "DRB1*09:02",
+            "DRB1*10:01", "DRB1*11:01", "DRB1*11:03", "DRB1*11:04", "DRB1*12:01", "DRB1*12:02", "DRB1*13:01", "DRB1*13:02",
+            "DRB1*13:03", "DRB1*13:05", "DRB1*14:01", "DRB1*14:02", "DRB1*14:03", "DRB1*14:04", "DRB1*14:54", "DRB1*15:01",
+            "DRB1*15:02", "DRB1*15:03", "DRB1*16:01", "DRB1*16:02", "DRB3*01:01", "DRB3*02:01", "DRB3*02:02", "DRB3*03:01",
+            "DRB4*01:01", "DRB4*01:03")
+
+`85V` <- c("DRB1*01:01", "DRB1*01:03", "DRB1*03:01", "DRB1*03:02", "DRB1*03:03", "DRB1*04:01", "DRB1*04:02", "DRB1*04:03",
+           "DRB1*04:04", "DRB1*04:05", "DRB1*07:01", "DRB1*08:01", "DRB1*08:02", "DRB1*09:01", "DRB1*09:02", "DRB1*10:01",
+           "DRB1*11:01", "DRB1*11:03", "DRB1*11:04", "DRB1*13:01", "DRB1*13:02", "DRB1*13:03", "DRB1*13:05", "DRB1*14:01",
+           "DRB1*14:02", "DRB1*14:03", "DRB1*14:04", "DRB1*14:54", "DRB1*15:01", "DRB1*15:02", "DRB1*15:03", "DRB1*16:01",
+           "DRB1*16:02", "DRB3*01:01", "DRB3*02:01", "DRB3*02:02", "DRB3*03:01", "DRB4*01:01", "DRB4*01:03", "DRB5*01:01")
+
+`78VS` <- c("DRB1*07:01", "DRB1*09:01")
+
+# 33N - other, non-antibody reactive, index max_oth, type oth
+ max <- mmv2_b %>% group_by(type) %>% mutate(max = max(index)) %>% ungroup() %>% select(type, max) %>% distinct()
+ max_abv <- max %>% filter(type == "AbV") %>% pull(max)
+ max_oth <- max %>% filter(type == "oth") %>% pull(max)
+
+tmp <- data.frame(names(mmv2_b)) %>%
+        setNames("name") %>%
+        mutate(add = ifelse(name %in% c("index"), max_oth,
+                            ifelse(name %in% c("type"), "oth",
+                                   ifelse(name %in% `33N`, "33N", "")))) %>%
+        t() %>%
+        as.data.frame() %>%
+        janitor::row_to_names(1) %>%
+        mutate(index = as.numeric(index) + 1)
+
+mmv2_b <- rbind(mmv2_b, tmp)
+
+rm(max, max_abv, max_oth, tmp)
+
+# 78Y - other, non-antibody reactive, index max_oth, type oth
+# find max numbers
+max <- mmv2_b %>% group_by(type) %>% mutate(max = max(index)) %>% ungroup() %>% select(type, max) %>% distinct()
+max_abv <- max %>% filter(type == "AbV") %>% pull(max)
+max_oth <- max %>% filter(type == "oth") %>% pull(max)
+
+tmp <- data.frame(names(mmv2_b)) %>%
+  setNames("name") %>%
+  mutate(add = ifelse(name %in% c("index"), max_oth,
+                      ifelse(name %in% c("type"), "oth",
+                             ifelse(name %in% `78Y`, "78Y", "")))) %>%
+  t() %>%
+  as.data.frame() %>%
+  janitor::row_to_names(1) %>%
+  mutate(index = as.numeric(index) + 1)
+
+mmv2_b <- rbind(mmv2_b, tmp)
+rm(max, max_abv, max_oth, tmp)
+
+# 108P - other, non-antibody reactive, index max_oth, type oth
+max <- mmv2_b %>% group_by(type) %>% mutate(max = max(index)) %>% ungroup() %>% select(type, max) %>% distinct()
+max_abv <- max %>% filter(type == "AbV") %>% pull(max)
+max_oth <- max %>% filter(type == "oth") %>% pull(max)
+
+tmp <- data.frame(names(mmv2_b)) %>%
+  setNames("name") %>%
+  mutate(add = ifelse(name %in% c("index"), max_oth,
+                      ifelse(name %in% c("type"), "oth",
+                             ifelse(name %in% `108P`, "108P", "")))) %>%
+  t() %>%
+  as.data.frame() %>%
+  janitor::row_to_names(1) %>%
+  mutate(index = as.numeric(index) + 1)
+
+mmv2_b <- rbind(mmv2_b, tmp)
+rm(max, max_abv, max_oth, tmp)
+
+# 85V - other, non-antibody reactive, index max_oth, type oth
+max <- mmv2_b %>% group_by(type) %>% mutate(max = max(index)) %>% ungroup() %>% select(type, max) %>% distinct()
+max_abv <- max %>% filter(type == "AbV") %>% pull(max)
+max_oth <- max %>% filter(type == "oth") %>% pull(max)
+
+tmp <- data.frame(names(mmv2_b)) %>%
+  setNames("name") %>%
+  mutate(add = ifelse(name %in% c("index"), max_oth,
+                      ifelse(name %in% c("type"), "oth",
+                             ifelse(name %in% `85V`, "85V", "")))) %>%
+  t() %>%
+  as.data.frame() %>%
+  janitor::row_to_names(1) %>%
+  mutate(index = as.numeric(index) + 1)
+
+mmv2_b <- rbind(mmv2_b, tmp)
+rm(max, max_abv, max_oth, tmp)
+
+# 78VS - antibody reactive, index max_abv, type AbV
+max <- mmv2_b %>% group_by(type) %>% mutate(max = max(index)) %>% ungroup() %>% select(type, max) %>% distinct()
+max_abv <- max %>% filter(type == "AbV") %>% pull(max)
+max_oth <- max %>% filter(type == "oth") %>% pull(max)
+
+tmp <- data.frame(names(mmv2_b)) %>%
+  setNames("name") %>%
+  mutate(add = ifelse(name %in% c("index"), max_abv,
+                      ifelse(name %in% c("type"), "AbV",
+                             ifelse(name %in% `78VS`, "78VS", "")))) %>%
+  t() %>%
+  as.data.frame() %>%
+  janitor::row_to_names(1) %>%
+  mutate(index = as.numeric(index) + 1)
+
+mmv2_b <- rbind(mmv2_b, tmp) %>% arrange(type, index)
+rm(max, max_abv, max_oth, tmp)
+
+write.csv(mmv2_b, "inst/extdata/matchmaker/check/mhcII_v2_B.csv", row.names = FALSE)
 
 
 
