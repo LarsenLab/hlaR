9a431cc19b62d3de83deaf0c5a19fb70b69ca4d7
xzhan50 <joan.zhang@emory.edu>
2021-03-29 15:43:55 -0400

    modify description of one of the paramater

--- R/eplet-MHC-I.R
+++ R/eplet-MHC-I.R
@@ -1,14 +1,14 @@
 #' @name CalEpletMHCI
 #' @title Calculate class I MHC eplet mismatch.
 #' @param dat_in
-#' A dataframe with subject info(first 2 columns) and class I MHC allele info.
+#' A dataframe with subject info(pair_id and subject_type) and both copies of each copy of HLA A, B, and C.
 #' Each unique participant id has 2 rows associated with it, 1 for recipient, 1 for donor.
 #' @param ver
 #' Version number of MatchMaker based eplet reference table to use. Mismatch calculation logic are based on MatchMaker too.
 #' @return
 #' A list of data tables.
 #' single_detail: single molecule level class I MHC eplet mismatch table, including mismatch eplet name and total count of each allele.
-#' overall_count: original input data appended with count of unique mismatched eplet.
+#' overall_count: original input data appended with total count of mismatched eplet.
 #' overall_detail: percentage of mismatch across all subject of each eplet.
 #' @export
 #'
