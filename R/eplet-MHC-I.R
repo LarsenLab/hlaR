78678e109d044c1dd4f9caf855e8d86a242045d9
xzhan50 <joan.zhang@emory.edu>
2021-04-05 15:35:37 -0400

    eplet mhcI/II: keep all records for allele with our without mismatch for a easier merge back to raw data

--- R/eplet-MHC-I.R
+++ R/eplet-MHC-I.R
@@ -226,7 +226,7 @@ CalEpletMHCI <- function(dat_in, ver = 3) {
     left_join(., id_match, by = "pair_id") %>%
     select(-pair_id) %>%
     rename(pair_id = pair_id_ori) %>%
-    filter(mm_cnt != 0) %>% # # keep mismatch eplets only
+    # filter(mm_cnt != 0) %>% # keep mismatch eplets only
     select(pair_id, everything())
   #* end of step 7 *#
 
