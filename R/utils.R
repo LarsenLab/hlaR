60d72d8e73db0c82113cd70efa63ef4720ecbadb
xzhan50 <joan.zhang@emory.edu>
2022-10-24 15:26:16 -0400

    CalRiskScr(): tweak logic to take care of all NA or identical situation

--- R/utils.R
+++ R/utils.R
@@ -33,49 +33,55 @@ GenerateLookup <- function(lkup_in, locus_in){
 #' @rdname utils
 CalRiskScr <- function(dat_scr) {
   risk_scr <- dat_scr %>%
-              filter(mm_eplets != "Not Found") %>%
-              mutate(hla = gsub("\\*.*", "", hla),
-                     grp = ifelse(hla %in% c("DRB1", "DRB3", "DRB4", "DRB5"), "DR",
-                                  ifelse(hla %in% c("DQA1", "DQB1") & haplotype_id %in% c("1"), "DQ1",
-                                         ifelse(hla %in% c("DQA1", "DQB1") & haplotype_id %in% c("2"), "DQ2", hla)))) %>%
-              filter(grp %in% c("DR", "DQ1", "DQ2") & mm_cnt > 0 ) %>%
-              group_by(grp) %>%
-              summarise(max1 = suppressWarnings(max(mm_cnt, na.rm=TRUE)),
-                        sum1 = sum(mm_cnt, na.rm=TRUE)) %>%
-              ungroup()  %>%
-              mutate(max1 = ifelse(grp %in% c("DQ1", "DQ2"), sum1, max1)) %>%
-              select(-sum1) %>%
-              mutate(locus = ifelse(grp %in% c("DR"), "DR", "DQ")) %>%
-              group_by(locus) %>%
-              summarise(score = suppressWarnings(max(max1, na.rm=TRUE))) %>%
-              ungroup() %>%
-              as.data.frame() %>%
-              rownames_to_column %>%
-              gather(var, value, -rowname) %>%
-              spread(rowname, value) %>%
-              janitor::row_to_names(1) %>%
-              as.data.frame()
+    filter(!(mm_eplets %in% c("Not Found", "", " "))) %>%
+    mutate(hla = gsub("\\*.*", "", hla),
+           grp = ifelse(str_detect(hla, "DRB"), "DR",
+                        ifelse(hla %in% c("DQA1", "DQB1") & haplotype_id %in% c("1"), "DQ1",
+                               ifelse(hla %in% c("DQA1", "DQB1") & haplotype_id %in% c("2"), "DQ2", hla)))) %>%
+    filter(grp %in% c("DR", "DQ1", "DQ2") & mm_cnt > 0 ) %>%
+    group_by(grp) %>%
+    summarise(max1 = suppressWarnings(max(mm_cnt, na.rm=TRUE)),
+              sum1 = sum(mm_cnt, na.rm=TRUE)) %>%
+    ungroup()  %>%
+    mutate(max1 = ifelse(grp %in% c("DQ1", "DQ2"), sum1, max1)) %>%
+    select(-sum1) %>%
+    mutate(locus = ifelse(grp %in% c("DR"), "DR", "DQ")) %>%
+    group_by(locus) %>%
+    summarise(score = suppressWarnings(max(max1, na.rm=TRUE))) %>%
+    ungroup() %>%
+    as.data.frame() %>%
+    rownames_to_column %>%
+    gather(var, value, -rowname) %>%
+    spread(rowname, value) %>%
+    janitor::row_to_names(1) %>%
+    as.data.frame()
 
-  # set DQ if no DQ
-  if(!("DQ" %in% names(risk_scr))) {
-    risk_scr$DQ = 0
+  if(dim(risk_scr)[1] == 0){
+    risk_scr <- data.frame(pair_id = NA, DQ = NA, DR = NA, risk = NA)
+  } else {
+
+    # set DQ if no DQ
+    if(!("DQ" %in% names(risk_scr))) {
+      risk_scr$DQ = 0
+    }
+
+    # set DR if no DR
+    if(!("DR" %in% names(risk_scr))) {
+      risk_scr$DR = 0
+    }
+
+    risk_scr <- risk_scr %>%
+      # change data type to numeric for risk calculation
+      mutate(DQ = as.numeric(DQ),
+             DR = as.numeric(DR)) %>%
+      mutate(risk = ifelse(between(DQ, 15, 31), "high",
+                           ifelse((DR >= 7 & DQ <= 14) | (DR < 7 & between(DQ, 9, 15)), "interm",
+                                  ifelse(DR < 7 & DQ < 9, "low", "out of bound"))))
   }
-
-  # set DR if no DR
-  if(!("DR" %in% names(risk_scr))) {
-    risk_scr$DR = 0
-  }
-
-  risk_scr <- risk_scr %>%
-              # change data type to numeric for risk calculation
-              mutate(DQ = as.numeric(DQ),
-                     DR = as.numeric(DR)) %>%
-              mutate(risk = ifelse(between(DQ, 15, 31), "high",
-                                   ifelse((DR >= 7 & DQ <= 14) | (DR < 7 & between(DQ, 9, 15)), "interm",
-                                          ifelse(DR < 7 & DQ < 9, "low", "out of bound"))))
 
   return(risk_scr)
 }
+
 
 #' @rdname utils
 FuncForCompHaplo <- function(tbl_raw, tbl_in) {
